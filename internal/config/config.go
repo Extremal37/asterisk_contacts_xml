536a155ac8447e42022edfc3240d1ed28bb002eb
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-07-21 13:01:00 +0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+func New() *Config {
+	return &Config{}
+}
+
+type Config struct {
+	DB
+	Log
+}
+
+type DB struct {
+	Username string `env:"DB_USERNAME" env-default:"asteriskuser"`
+	Password string `env:"DB_PASSWORD" env-default:"asteriskpassword"`
+	Host     string `env:"DB_HOST" env-default:"localhost"`
+	Port     string `env:"DB_PORT" env-default:"3306"`
+	Database string `env:"DB_DATABASE" env-default:"asterisk"`
+}
+
+type Log struct {
+	Level string `env:"LOG_LEVEL" env-default:"INFO"`
+}
+
+func (c *Config) ParseConfig() error {
+	if err := cleanenv.ReadEnv(c); err != nil {
+		return fmt.Errorf("failed to read env's :%w", err)
+	}
+	return nil
+}
