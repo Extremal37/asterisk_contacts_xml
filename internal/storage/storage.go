8edf07558c16235d7ba16b0967e8b09d07a0709e
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-07-21 13:57:25 +0300

    add storage

---
+++ internal/storage/storage.go
@@ -0,0 +1,68 @@
+package storage
+
+import (
+	"context"
+	"database/sql"
+	"github.com/Extremal37/asterisk_contacts_xml/internal/entity"
+	"github.com/Extremal37/asterisk_contacts_xml/internal/logger"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+const connTimeout = 3 * time.Minute
+
+type Storage struct {
+	conn *sql.DB
+	log  *logger.Logger
+}
+
+// New receive sql connection and logger and return Storage
+func New(conn *sql.DB, log *logger.Logger) *Storage {
+	return &Storage{
+		conn: conn,
+		log:  log,
+	}
+}
+
+func (s *Storage) Contacts(ctx context.Context) ([]entity.ContactDB, error) {
+	// Create context with timeout defined in connTimeout
+	c, cancel := context.WithTimeout(ctx, connTimeout)
+	defer cancel()
+
+	//Prepare query
+	var contacts []entity.ContactDB
+	query := "SELECT name,callerid FROM subscribers"
+
+	// Fetching rows from DB
+	rows, err := s.conn.QueryContext(c, query)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := rows.Close(); err != nil {
+			s.log.Errorf("Unable to close rows : %v", err)
+		}
+	}()
+
+	// Scan rows and put data to contacts array
+	for rows.Next() {
+		row := entity.ContactDB{}
+		err = rows.Scan(&row)
+		if err != nil {
+			s.log.Warnf("Unable to scan row: %v", err)
+			continue
+		}
+		contacts = append(contacts, row)
+	}
+
+	return contacts, nil
+}
+
+func (s *Storage) Close() {
+	err := s.conn.Close()
+	if err != nil {
+		s.log.Warnf("Failed to close DB connection: %v", err)
+	} else {
+		s.log.Debug("DB connection closed")
+	}
+}
