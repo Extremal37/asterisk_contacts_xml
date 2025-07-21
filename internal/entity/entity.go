4dd93d8ed5232dcb44e2d9a854f65b6ab991ee92
Dmitry Tumalanov <tumalanov37@gmail.com>
2025-07-21 16:06:37 +0300

    add xml entity

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -1,6 +1,19 @@
 package entity
 
+import "encoding/xml"
+
 type ContactDB struct {
 	number string `db:"name"`
 	name   string `db:"callerid"`
+}
+
+type ContactXML struct {
+	XMLName      xml.Name `xml:"contact"`
+	Name         string   `xml:"name,attr"`
+	Bandwidth    uint8    `xml:"bandwidth,attr"`
+	Group        uint8    `xml:"group,attr"`
+	Favorite     uint8    `xml:"favorite,attr"`
+	Sticky       uint8    `xml:"sticky,attr"`
+	Favoritetype uint8    `xml:"favoritetype,attr"`
+	Number       string   `xml:"number"`
 }
